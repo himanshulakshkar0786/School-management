ff45ce66ce765705be2e8d705987a58e0865d495
Himanshu Lakhera <himanshulakshkar0786@gmail.com>
2018-02-03 16:22:59 +0530

    updates

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,5 +4,5 @@ class Student < ApplicationRecord
 	belongs_to :classname
 	has_attached_file :image, styles: { small: "20*20", large: "300*300"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	has_attached_file :video, styles: { medium: { gemoetry: '640*480', format: 'mp4'}, thumb: { gemoetry: '160*120', format: 'jpeg', time: 5}}, processors: [:transcoder]
+	has_attached_file :video
 end
